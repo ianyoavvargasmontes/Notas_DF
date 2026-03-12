0fe3838821b5d4de07d1578143ddaad9f0a403c8
ianyoavvargasmontes-dotcom <ianyoav.vargasmontes@aulavirtual.umar.mx>
2026-03-12 17:54:43 -0600

    f

--- script.js
+++ script.js
@@ -6,40 +6,41 @@ let contador = 1;
 const colores = ["red","blue","green","orange","purple","pink","cyan"];
 
 
-/* EVENTO DELEGADO EN EL CONTENEDOR */
+/* EVENTO DELEGADO EN LA GALERIA */
 galeria.addEventListener("click", (e) => {
 
-    if (e.target.classList.contains("item")) {
+    if(e.target.classList.contains("item")){
 
         const color = e.target.dataset.color;
 
-        e.target.style.backgroundColor = color;
-
-        console.log("Color aplicado:", color);
+        /* cambiar color de la galeria */
+        galeria.style.backgroundColor = color;
 
     }
 
 });
 
 
-/* CREACIÓN DINÁMICA DE CAJAS */
+/* CREAR NUEVAS CAJAS */
 btnAgregar.addEventListener("click", () => {
 
     const nuevo = document.createElement("div");
 
     nuevo.className = "item";
-
     nuevo.innerText = "Caja " + contador;
 
     nuevo.style.padding = "10px";
     nuevo.style.border = "1px solid black";
     nuevo.style.cursor = "pointer";
 
-    /* GENERAR COLOR ALEATORIO */
-    const colorAleatorio = colores[Math.floor(Math.random() * colores.length)];
+    /* generar color aleatorio */
+    const colorAleatorio = colores[Math.floor(Math.random()*colores.length)];
 
-    /* GUARDAR EN DATASET */
+    /* guardar color */
     nuevo.dataset.color = colorAleatorio;
+
+    /* aplicar color a la caja */
+    nuevo.style.backgroundColor = colorAleatorio;
 
     galeria.appendChild(nuevo);
 
